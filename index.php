b8f6057b8de332a113d9b7dc8bac53dd4c576455
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-06-04 22:58:53 +0200

    sort songs by name

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require('lib/common.php');
 
 $twig = twigloader();
 
-$allSongs = query("SELECT * FROM music");
+$allSongs = query("SELECT * FROM music ORDER BY name ASC");
 
 echo $twig->render('index.twig', [
 	'all_songs' => $allSongs
